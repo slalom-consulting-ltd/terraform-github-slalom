7d479feaaa17093cc62c3757c60cedacf6b66451
James Woolfenden <James.Woolfenden@slalom.com>
2019-10-25 14:28:08 +0100

    cleanup

--- module.github.tf
+++ module.github.tf
@@ -1,8 +1,8 @@
 module "github" {
-   source="../terraform-github-team"
-   common_tags     = "${var.common_tags}" 
-   team_name       = "${var.team_name}"
-   team_description= "${var.team_description}"
-   team_privacy    = "${var.team_privacy}"
-   users           = "${var.users}"
+   source          ="github.com/slalom-consulting-ltd/terraform-github-team?ref=0.0.2"
+   common_tags     = var.common_tags
+   team_name       = var.team_name
+   team_description= var.team_description
+   team_privacy    = var.team_privacy
+   users           = var.users
 }
